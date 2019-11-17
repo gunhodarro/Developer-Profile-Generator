6401c77295a46fb86eba13e86150a3520939b03d
matthew juretic <gunhodarro@gmail.com>
2019-11-16 17:59:41 -0700

    working almost fully need to tweak a few things yet

--- index.js
+++ index.js
@@ -1,11 +1,74 @@
-const questions = [
-  
-];
+const inquirer = require("inquirer");
+const fs = require("fs");
+const prompt = require("prompt");
+const axios = require("axios");
+const htmlgen = require("./generateHTML");
+const util = require("util");
+const writeFileAsync = util.promisify(fs.writeFile);
+let pdf = require('html-pdf');
+let html = fs.readFileSync('./portfolio.html', 'utf8');
 
-function writeToFile(fileName, data) {
- 
+function promptUser(){
+ return inquirer.prompt([
+      {
+        type: "list",
+        name: "brColor",
+        message: "What is your favorite color?",
+        choices: ["red","blue", "green", "pink"]
+      },
+      {
+        type: "input",
+        name: "username",
+        message: "Enter your GitHub username:"
+      },
+    ]).then(function({ username, brColor }){
+
+        const queryUrl = `https://api.github.com/users/${username}`;
+
+        axios.get(queryUrl).then(function(res) {
+           
+            const gitUserName = res.data.login;
+            const gitProfileImg = res.data.avatar_url;
+            const userLoca = res.data.location;
+            const userGitProfile = res.data.html_url;
+            const userBlog = res.data.blog;
+            const userBio = res.data.bio;
+            const repoNum = res.data.public_repos;
+            const followerNum = res.data.followers;
+            const starsNum = res.data.following;
+            const followingNum = res.data.following;
+
+            console.log(gitUserName);
+            console.log(gitProfileImg);
+            console.log("https://www.google.com/maps/search/?api=1&query="+userLoca);
+            console.log(userGitProfile);
+            console.log("https://"+userBlog);
+            console.log(userBio);
+            console.log(repoNum);
+            console.log(followerNum);
+            console.log(starsNum);
+            console.log(followingNum);
+
+           
+     console.log(brColor);
+            fs.writeFile("portfolio.html", htmlgen(res, brColor), function(err) {
+            
+              if (err) {
+                return console.log(err);
+              }
+            
+              console.log("Successful!");
+
+              pdf.create(html).toFile('./portfolio.pdf', function(err, res) {
+                if (err) return console.log(err);
+                console.log(res);
+              })           
+            
+            })
+
+        })
+    })
+
 }
 
-function init() {
-
-init();
+promptUser();
